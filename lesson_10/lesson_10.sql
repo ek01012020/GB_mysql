dd2b770a04d2703d5de1af887a8f9c6696a3b3fd
Helen <ek01012020@gmail.com>
2020-08-18 11:52:42 +0300

    lesson_10

---
+++ lesson_10/lesson_10.sql
@@ -0,0 +1,41 @@
+-- Проанализировать какие запросы могут выполняться наиболее часто в процессе работы приложения и добавить необходимые индексы.
+	
+SHOW TABLES;
+SHOW INDEX FROM users;
+SELECT * FROM users limit 1;
+
+CREATE INDEX media_filename_idx ON media (filename);
+CREATE INDEX messages_is_important_idx ON messages (is_important);
+CREATE INDEX posts_views_counter_idx ON posts (views_counter);
+
+CREATE UNIQUE INDEX profiles_city_country_uq 
+	ON profiles (city, country);
+CREATE INDEX users_first_name_last_name_idx
+	ON users(first_name, last_name);
+
+
+-- Построить запрос, который будет выводить следующие столбцы:
+-- имя группы
+-- среднее количество пользователей в группах
+-- самый молодой пользователь в группе
+-- самый старший пользователь в группе
+-- общее количество пользователей в группе
+-- всего пользователей в системе
+-- отношение в процентах (общее количество пользователей в группе / всего пользователей в системе) * 100
+	
+SELECT DISTINCT c.name,
+		COUNT(cu.user_id ) OVER () / (SELECT COUNT(id) FROM communities c2 ) AS `AVG` ,
+		MAX(p.birthday ) OVER w AS young, 
+		MIN(p.birthday ) OVER w AS `old`, 
+		COUNT(cu.user_id ) OVER w AS count_users,
+		COUNT(p.user_id ) OVER () AS total_users,
+		COUNT(cu.user_id ) OVER w / COUNT(p.user_id ) OVER () * 100 AS `%` 
+FROM (communities_users cu
+	JOIN
+	communities c 
+		ON cu.community_id = c.id		
+	JOIN 
+	profiles p 
+		ON cu.user_id = p.user_id)
+		WINDOW w AS (PARTITION BY cu.community_id );
+	
